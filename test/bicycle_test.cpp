d07a50261e8093c0583a59b8953fa7430082303b
ManifoldFR <wilson.jallet@polytechnique.org>
2024-04-29 20:07:34 +0200

    Add missing to_json() converter for Eigen

--- test/bicycle_test.cpp
+++ test/bicycle_test.cpp
@@ -24,6 +24,30 @@ using json = nlohmann::json;
 
 using namespace altro;
 
+namespace Eigen {
+template<typename Scalar, int Rows, int Cols>
+void to_json(nlohmann::json& j, const Matrix<Scalar, Rows, Cols>& matrix) {
+  for (int row = 0; row < matrix.rows(); ++row) {
+    nlohmann::json column = nlohmann::json::array();
+    for (int col = 0; col < matrix.cols(); ++col) {
+      column.push_back(matrix(row, col));
+    }
+    j.push_back(column);
+  }
+}
+
+template<typename Scalar, int Rows, int Cols>
+void from_json(const nlohmann::json& j, Matrix<Scalar, Rows, Cols>& matrix) {        
+  for (std::size_t row = 0; row < j.size(); ++row) {
+    const auto& jrow = j.at(row);
+    for (std::size_t col = 0; col < jrow.size(); ++col) {
+      const auto& value = jrow.at(col);
+      value.get_to(matrix(row, col));
+    }
+  }
+}
+}
+
 TEST(BicycleTests, Dynamics) {
   BicycleModel model;
   int n = BicycleModel::NumStates;
